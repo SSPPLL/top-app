ee11ee81a15fef229aa428bda8fd2d07a50201a9
SSPPLL <arturdubovskiy@gmail.com>
2025-06-16 18:51:00 +0300

    fix config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 	// output: 'export',
 	images: {
-		remotePatterns: [new URL(`${process.env.PRODUCTS_IMAGES_PATH}**`)],
+		remotePatterns: [new URL(`https://old-images.hb.ru-msk.vkcs.cloud/uploads/**`)],
 	},
 	webpack(config) {
 		config.module.rules.push({
